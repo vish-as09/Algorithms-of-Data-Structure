00cdb5f0d03ea1de2790b67bc9f3aaba6e71f741
vish-as09 <rumjhumsingh355@gmail.com>
2026-04-16 12:55:26 +0530

    diamond shaped matrix

---
+++ MATRIX.java
@@ -0,0 +1,35 @@
+
+public class MATRIX {
+
+	public static void main(String[] args) {
+		for(int i=1 ; i<=3;i++) {
+			for(int j=3 ; j>i; j--) {
+				System.out.print("-"+" ");
+			}
+			for(int j=0;j<i;j++) {
+				System.out.print("*"+" ");
+			}for(int j=1 ; j<i; j++) {
+				System.out.print("*"+" ");
+			}
+			for(int j=3;j>i;j--) {
+				System.out.print("-"+" ");
+			}
+			System.out.println();
+		}
+		for(int i=2 ; i>0;i--) {
+			for(int j=3 ; j>i; j--) {
+				System.out.print("-"+" ");
+			}
+			for(int j=0;j<i;j++) {
+				System.out.print("*"+" ");
+			}
+			for(int j=1 ; j<i; j++) {
+				System.out.print("*"+" ");
+			}
+			for(int j=3;j>i;j--) {
+				System.out.print("-"+" ");
+			}System.out.println();
+		}
+	}
+
+}
